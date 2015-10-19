b90992ddf1c1ef54cc21da18f4a99df526bd5907
yagao0o <yagao0o@126.com>
2015-10-19 16:55:47 +0800

    Merge all method into single one to get userInfo from appPlatform

--- src/android/AppPlatformPlugin.java
+++ src/android/AppPlatformPlugin.java
@@ -12,19 +12,16 @@ import com.inspur.zsyw.platform.Platform.PlatformCallback;
 public class AppPlatformPlugin extends CordovaPlugin {
 	private String packageName = "";
 	private Platform platform;
-	private boolean isSuccess = false;
-	private String msg = "";
+	private CallbackContext cb;
 	
 	PlatformCallback callback = new PlatformCallback() {
 		@Override
 		public void onSuccess(String response) {
-			isSuccess = true;
-			msg = response;
+			cb.success(response);
 		}
 		@Override
 		public void onFailure(String response) {
-			isSuccess = false;
-			msg = response;
+			cb.error(response);
 		}
 		@Override
 		public void onServiceConnected() {
@@ -35,20 +32,10 @@ public class AppPlatformPlugin extends CordovaPlugin {
 	public boolean execute(String action, JSONArray args, final CallbackContext cb) throws JSONException {
 		
 		if(action.equals("setPackageName")){
+			this.cb = cb;
 			this.packageName = args.getString(0);
 			platform = Platform.getInstance(this.cordova.getActivity().getApplicationContext(), callback);
 			platform.bindService();
-			cb.success("Seems good! " + platform.toString());
-			return true;
-		}
-		
-		if(action.equals("getInfo")){
-			if(isSuccess){
-				cb.success(msg);
-			}
-			else{
-				cb.error(msg);
-			}
 			return true;
 		}
 		
--- www/appplatform.js
+++ www/appplatform.js
@@ -3,11 +3,5 @@ var AppPlatform = {};
 AppPlatform.setPackageName = function(name, cb){
 	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','setPackageName',[name])
 };
-// AppPlatform.init = function(cb){
-// 	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','init',[])
-// };
-AppPlatform.getInfo = function(cb){
-	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','getInfo',[])
-};
 
 module.exports = AppPlatform;
