aa0c24da609884722d7dd5bdd8ee3303bc68b2f1
yagao0o <yagao0o@126.com>
2016-11-29 18:43:59 +0800

    Update to 3.0

---
+++ README.md
@@ -0,0 +1,13 @@
+### 安装
+``cordova plugin add MY-PLUGIN-NAME``
+### 使用方法：
+- cordova 2:
+``
+window.plugins.AppPlatform.setPackageName('instance-name', callback)
+``
+
+- typescript:
+
+``
+window.plugins.AppPlatform.setPackageName('instance-name', callback)
+``
--- src/android/AppPlatformPlugin.java
+++ src/android/AppPlatformPlugin.java
@@ -13,11 +13,17 @@ public class AppPlatformPlugin extends CordovaPlugin {
 	private String packageName = "";
 	private Platform platform;
 	private CallbackContext cb;
-	
+  private String type = "connect";
+
 	PlatformCallback callback = new PlatformCallback() {
 		@Override
 		public void onSuccess(String response) {
-			cb.success(response);
+      if ("connect".equals(type)) {
+        // getInstance callback do Nothing
+      }
+      else{
+        cb.success(response);
+      }
 		}
 		@Override
 		public void onFailure(String response) {
@@ -25,20 +31,22 @@ public class AppPlatformPlugin extends CordovaPlugin {
 		}
 		@Override
 		public void onServiceConnected() {
+      this.type = "userInfo";
 			platform.getUserInfo(packageName);
 		}
 	};
-	
+
 	public boolean execute(String action, JSONArray args, final CallbackContext cb) throws JSONException {
-		
-		if(action.equals("setPackageName")){
+
+		if("setPackageName".equals(action)){
 			this.cb = cb;
 			this.packageName = args.getString(0);
+      this.type = "connect";
 			platform = Platform.getInstance(this.cordova.getActivity().getApplicationContext(), callback);
 			platform.bindService();
 			return true;
 		}
-		
+
 		return false;
-	}	
+	}
 }
