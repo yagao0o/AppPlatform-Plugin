d8e134031d04e4d9afc71a642e01ddf5b47017bd
yagao0o <yagao0o@126.com>
2017-07-21 16:58:57 +0800

    Add call photo take/voice recorder/print label App

--- src/android/AppPlatformPlugin.java
+++ src/android/AppPlatformPlugin.java
@@ -5,6 +5,11 @@ import org.json.*;
 import java.util.*;
 import android.content.*;
 import android.util.*;
+import android.os.Bundle;
+// import java.util.List;
+import android.widget.Toast;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 
 import droid.app.hp.api.platform.Platform;
 import droid.app.hp.api.platform.Platform.PlatformCallback;
@@ -45,9 +50,99 @@ public class AppPlatformPlugin extends CordovaPlugin {
 			platform = Platform.getInstance(this.cordova.getActivity().getApplicationContext(), callback);
 			platform.bindService();
 			return true;
-		} else if("getTel".equals(action)) {
-			String brobandTel = this.cordova.getActivity().getIntent().getStringExtra("brobandTel");
-			cb.success(brobandTel);
+		} else if("getToken".equals(action)) {
+			String token = this.cordova.getActivity().getIntent().getStringExtra("token");
+			cb.success(token);
+			return true;
+		} else if("startCamera".equals(action)) {//启动掌上拍照
+			String formNo = args.getString(0);//工单编号
+			String flowId = args.getString(1);//工单流水号
+			String htName = args.getString(2);//环节名称
+			String tkiid = args.getString(3);//工作项ID
+			String templateName = args.getString(4);//工单分类
+			String token = args.getString(5);//工单分类
+			boolean isReupload = args.getBoolean(6);//是否重新上传
+			Bundle bundle = new Bundle();
+			bundle.putString("formNo", formNo);
+			bundle.putString("flowId", flowId);
+			bundle.putString("htName", htName);
+			bundle.putString("tkiid", tkiid);
+			bundle.putString("templateName", templateName);
+			bundle.putString("token", token);
+			bundle.putBoolean("isReupload", isReupload);
+			Intent intent = new Intent();
+			intent.addCategory(Intent.CATEGORY_DEFAULT);
+			intent.setComponent(new ComponentName(
+							"com.inspur.cameraupload",
+							"com.inspur.cameraupload.login.LoginActivity"));
+			intent.putExtras(bundle);
+			intent.putExtra("zsyw", "zsyw");
+			PackageManager pm = this.cordova.getActivity().getPackageManager();
+			List<ResolveInfo> activities = pm.queryIntentActivities(intent,
+							PackageManager.MATCH_DEFAULT_ONLY);
+			if (activities == null || activities.size() < 1) {
+				  Toast.makeText(this.cordova.getActivity(), "未安装\"掌上拍照\"", Toast.LENGTH_LONG).show();
+					return true;
+			}
+			this.cordova.getActivity().startActivity(intent);
+			return true;
+		} else if("startCallRecorder".equals(action)) {//启动录音
+			String formNo = args.getString(0);//工单编号
+			String flowId = args.getString(1);//工单流水号
+			String htName = args.getString(2);//环节名称
+			String tkiid = args.getString(3);//工作项ID
+			String contacterTel = args.getString(4);//联系人电话
+			String templateName = args.getString(5);//工单分类
+			String token = args.getString(6);//工单分类
+			Bundle bundle = new Bundle();
+			bundle.putString("formNo", formNo);
+			bundle.putString("flowId", flowId);
+			bundle.putString("htName", htName);
+			bundle.putString("tkiid", tkiid);
+			bundle.putString("contacterTel", contacterTel);
+			bundle.putString("templateName", templateName);
+			bundle.putString("token", token);
+			Intent intent = new Intent();
+			intent.addCategory(Intent.CATEGORY_DEFAULT);
+			intent.setComponent(new ComponentName(
+							"com.inspur.callrecorder",
+							"com.inspur.callrecorder.login.LoginActivity"));
+			intent.putExtras(bundle);
+			intent.putExtra("zsyw", "zsyw");
+			PackageManager pm = this.cordova.getActivity().getPackageManager();
+			List<ResolveInfo> activities = pm.queryIntentActivities(intent,
+							PackageManager.MATCH_DEFAULT_ONLY);
+			if (activities == null || activities.size() < 1) {
+				  Toast.makeText(this.cordova.getActivity(), "未安装\"通话录音\"", Toast.LENGTH_LONG).show();
+					return true;
+			}
+			this.cordova.getActivity().startActivity(intent);
+			return true;
+		} else if("startLabelPrint".equals(action)) {//启动标签打印
+			String houseId4Show = args.getString(0);//房号ID
+			String houseName4Show = args.getString(1);//房号信息
+			String hold_device_id4show = args.getString(2);//设备ID
+			String hold_device_name4show = args.getString(3);//设备信息
+			Bundle bundle = new Bundle();
+			bundle.putString("houseId4Show", houseId4Show);
+			bundle.putString("houseName4Show", houseName4Show);
+			bundle.putString("hold_device_id4show", hold_device_id4show);
+			bundle.putString("hold_device_name4show", hold_device_name4show);
+			Intent intent = new Intent();
+			intent.addCategory(Intent.CATEGORY_DEFAULT);
+			intent.setComponent(new ComponentName(
+							"com.label.print",
+							"com.label.print.login.LoginActivity"));
+			intent.putExtras(bundle);
+			intent.putExtra("zsyw", "zsyw");
+			PackageManager pm = this.cordova.getActivity().getPackageManager();
+			List<ResolveInfo> activities = pm.queryIntentActivities(intent,
+							PackageManager.MATCH_DEFAULT_ONLY);
+			if (activities == null || activities.size() < 1) {
+				  Toast.makeText(this.cordova.getActivity(), "未安装\"标签打印\"", Toast.LENGTH_LONG).show();
+					return true;
+			}
+			this.cordova.getActivity().startActivity(intent);
 			return true;
 		}
 
--- www/appplatform.js
+++ www/appplatform.js
@@ -3,8 +3,17 @@ var AppPlatform = {};
 AppPlatform.setPackageName = function(name, cb){
 	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','setPackageName',[name])
 };
-AppPlatform.getTel = function(name, cb){
-	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','getTel',[name])
+AppPlatform.getToken = function(cb){
+	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','getToken',[])
+};
+AppPlatform.startCamera = function(formNo,flowId,htName,tkiid,templateName,token,isReupload, cb){
+	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','startCamera',[formNo,flowId,htName,tkiid,templateName,token,isReupload])
+};
+AppPlatform.startCallRecorder = function(formNo,flowId,htName,tkiid,contacterTel,templateName,token, cb){
+	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','startCallRecorder',[formNo,flowId,htName,tkiid,contacterTel,templateName,token])
+};
+AppPlatform.startLabelPrint = function(houseId4Show,houseName4Show,hold_device_id4show,hold_device_name4show, cb){
+	cordova.exec(function(result){cb(null,result)},cb, 'AppPlatform','startLabelPrint',[houseId4Show,houseName4Show,hold_device_id4show,hold_device_name4show])
 };
 
 module.exports = AppPlatform;
